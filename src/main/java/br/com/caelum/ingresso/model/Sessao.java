91999332adcdaf7c200db5954a9954a8376202f6
marcelo <marcelomcamara@gmail.com>
2018-11-10 16:43:06 -0200

    work

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -39,4 +39,36 @@ public class Sessao {
 		return this.horario.plusMinutes(filme.getDuracao().toMinutes());
 	}
 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public LocalTime getHorario() {
+		return horario;
+	}
+
+	public void setHorario(LocalTime horario) {
+		this.horario = horario;
+	}
+
+	public Sala getSala() {
+		return sala;
+	}
+
+	public void setSala(Sala sala) {
+		this.sala = sala;
+	}
+
+	public Filme getFilme() {
+		return filme;
+	}
+
+	public void setFilme(Filme filme) {
+		this.filme = filme;
+	}
+
 }
---
+++ src/main/java/br/com/caelum/ingresso/validacao/GerenciadorDeSessao.java
@@ -0,0 +1,41 @@
+package br.com.caelum.ingresso.validacao;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import br.com.caelum.ingresso.model.Sessao;
+
+public class GerenciadorDeSessao {
+
+	private List<Sessao> sessoesDaSala;
+
+	public GerenciadorDeSessao(List<Sessao> sessoesDaSala) {
+		this.sessoesDaSala = sessoesDaSala;
+	}
+
+	private boolean horarioIsConflitante(Sessao sessaoExistente,
+			Sessao sessaoNova) {
+
+		LocalDate hoje = LocalDate.now();
+
+		LocalTime horarioSessaoExistente = sessaoExistente.getHorario()
+				.atDate(hoje).toLocalTime();
+		LocalTime horarioSessaoNova = sessaoNova.getHorario().atDate(hoje)
+				.toLocalTime();
+
+		boolean terminaAntes = sessaoNova.getHorarioTermino().isBefore(
+				horarioSessaoExistente);
+		boolean comecaDepois = sessaoExistente.getHorarioTermino().isBefore(
+				horarioSessaoNova);
+		if (terminaAntes || comecaDepois) {
+			return false;
+		}
+		return true;
+	}
+
+	public boolean cabe(Sessao sessaoNova) {
+		
+		return sessoesDaSala.stream().noneMatch(sessaoExistente -> horarioIsConflitante(sessaoExistente, sessaoNova));
+	}
+}
