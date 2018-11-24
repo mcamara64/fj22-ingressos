05590e3ea396fea187abdcfbdd7b240c48a99341
marcelo <marcelomcamara@gmail.com>
2018-11-24 10:52:07 -0200

    Adiciona preco em filme, sala e sessao

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -1,5 +1,7 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalTime;
 
 import javax.persistence.Entity;
@@ -20,6 +22,8 @@ public class Sessao {
 
 	@ManyToOne
 	private Filme filme;
+	
+	private BigDecimal preco=BigDecimal.ZERO;
 
 	/**
 	 * @deprecated hibernate only
@@ -33,6 +37,7 @@ public class Sessao {
 		this.horario = horario;
 		this.filme = filme;
 		this.sala = sala;
+		this.preco=sala.getPreco().add(filme.getPreco());
 	}
 
 	public LocalTime getHorarioTermino() {
@@ -49,7 +54,7 @@ public class Sessao {
 
 	public LocalTime getHorario() {
 		return horario;
-	}
+	}			
 
 	public void setHorario(LocalTime horario) {
 		this.horario = horario;
@@ -70,5 +75,13 @@ public class Sessao {
 	public void setFilme(Filme filme) {
 		this.filme = filme;
 	}
+	
+	public BigDecimal getPreco() {
+    	return preco.setScale(2, RoundingMode.HALF_UP);
+    }
+    
+    public void setPreco(BigDecimal preco) {
+    	this.preco=preco;
+    }
 
 }
